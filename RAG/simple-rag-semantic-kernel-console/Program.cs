7e310994c4717cc0ac8f7510b1db7eb126077da7
Christian Weyer <cw@thinktecture.com>
2024-02-09 11:25:23 +0100

    Adding one more sample

---
+++ RAG/simple-rag-semantic-kernel-console/Program.cs
@@ -0,0 +1,102 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using Microsoft.SemanticKernel.Connectors.Memory.Sqlite;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Plugins.Memory;
+
+const string COLLECTION = "documentation";
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false)
+    .AddUserSecrets<Program>()
+    .Build();
+
+var index = !File.Exists("index.db");
+var store = await SqliteMemoryStore.ConnectAsync("index.db");
+
+var memory = new MemoryBuilder()
+    .WithOpenAITextEmbeddingGenerationService("text-embedding-ada-002", 
+        config["OpenAi:ApiKey"] ?? throw new ArgumentNullException(nameof(config), "OpenAi:ApiKey is null."))
+    .WithMemoryStore(store)
+    .Build();
+
+var kernel = Kernel.Builder
+    .WithOpenAIChatCompletionService("gpt-3.5-turbo-16k", 
+        config["OpenAi:ApiKey"] ?? throw new ArgumentNullException(nameof(config), "OpenAi:ApiKey is null."))
+    .Build();
+
+kernel.ImportSemanticFunctionsFromDirectory("plugins", "qa");
+
+if (index)
+{
+    await Index();
+}
+
+while (true)
+{
+    Console.Write("Enter a question or press enter to quit: ");
+    var input = Console.ReadLine();
+    
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        Console.WriteLine("Bye 👋");
+        break;
+    }
+    
+    var answer = await Answer(input);
+    Console.WriteLine($"🤖:{answer}");
+}
+
+async Task<string> Answer(string question)
+{
+    var results = await memory.SearchAsync(COLLECTION, question, limit: 2).ToListAsync();
+    var variables = new ContextVariables(question)
+    {
+        ["context"] = results.Any() 
+            ? string.Join("\n", results.Select(r => r.Metadata.Text)) 
+            : "No context found for this question."
+    };
+    
+    var result = await kernel.RunAsync(variables, kernel.Functions.GetFunction("qa", "answer"));
+    
+    return result.GetValue<string>() ?? string.Empty;
+}
+
+async Task Index()
+{
+    Console.WriteLine("Indexing urls");
+    var urls = config.GetSection("urls").Get<IndexUrl[]>() ?? Array.Empty<IndexUrl>();
+    
+    using var client = new HttpClient();
+    
+    foreach (var url in urls)
+    {
+        Console.WriteLine($"Indexing {url.Url}");
+        await IndexUrl(client, url.Url, url.Selector);
+    }
+    
+    Console.WriteLine("Indexing done");
+}
+
+async Task IndexUrl(HttpClient client, string url, string contentSelector)
+{
+    var content = await client.GetStringAsync(url);
+    var title = string.Empty;
+    
+    if (!string.IsNullOrWhiteSpace(contentSelector))
+    {
+        var doc = new HtmlDocument();
+        doc.LoadHtml(content);
+        var mainElement = doc.DocumentNode.SelectSingleNode(contentSelector);
+        title = mainElement.SelectSingleNode("//h1").InnerText;
+        content = Cleanup(mainElement.InnerText);
+    }
+    
+    await memory.SaveInformationAsync(COLLECTION, content, url, title);
+    
+    static string Cleanup(string content) => content.Replace("\t", "").Replace("\r\n\r\n", "");
+}
+
+record IndexUrl(string Url, string Selector);
---
+++ RAG/simple-rag-semantic-kernel-console/README.md
@@ -0,0 +1,59 @@
+# Question & Answer flow with C# & Semantic Kernel
+
+A sample implementation of a question & answer flow using [Semantic Kernel](https://github.com/microsoft/semantic-kernel).
+
+This uses sqlite to store embeddings (caution: sqlite is not vector-optimized!) and OpenAI GPT to answer questions based on the text found in the database. The database is populated based on URLs from the configuration.
+
+## How to run
+
+- Get an [OpenAI API Key](https://platform.openai.com/account/api-keys)
+- Copy & paste the API key into the `appsettings.json` or user secrets (`OpenAI:ApiKey`)
+- Optionally adjust the input data in the `appsettings.json` (entry `Urls`)
+- `dotnet run`
+
+## Examples (based on the default content URLs in the sample code) 
+
+```
+Enter a question or press enter to quit: What is sk?
+🤖: Semantic Kernel (SK) is a lightweight SDK that lets you easily mix conventional programming languages with the latest in Large Language Model (LLM) AI "prompts" with templating, chaining, and planning capabilities out-of-the-box.
+```
+
+```
+Enter a question or press enter to quit: What systems cann SK connect to?
+🤖: SK can connect to external APIs, MS Graph Connector Kit, Bing search query, OpenXML streams, and SQLite.
+```
+
+```
+Enter a question or press enter to quit: Shoe me an example of creating a kernel
+🤖: using Microsoft.SemanticKernel;
+
+var myKernel = Kernel.Builder.Build();
+```
+
+```
+Enter a question or press enter to quit: Was bedeutet SK?
+🤖: SK steht für Semantic Kernel.
+```
+
+```
+Enter a question or press enter to quit: ¿Qué significa SK?
+🤖: SK significa Kernel Semántico.
+```
+
+## How does it work?
+
+- At the first run, the program will download a couple of pages of the Semantic Kernel documentation (configured in `appsettings.json`).
+- For each page, embeddings will be calculated using the [OpenAI Embeddings model](https://platform.openai.com/docs/guides/embeddings).
+- The content of the page and the embeddings are stored in a sqlite database.
+- When asked a question, embeddings will be calculated for the question.
+- The sqlite database will be searched be similar embeddings.
+- The top 2 results will be concatenated and sent to OpenAI with the following prompts:
+
+```
+Use the following pieces of context to answer the question at the end. If you don't know the answer, don't try to make up an answer and answer with 'I don't know'. Answer in the langauge that used for the question.
+
+{{$context}}
+
+Question: {{$input}}
+Answer:
+```
